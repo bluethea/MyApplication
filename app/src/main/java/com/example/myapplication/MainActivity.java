6d8e69ca04b1c4c19e16765cd360c32e3dd88ac3
bluethea <nazar.lukiashko@knf.stud.vu.lt>
2022-09-07 11:22:59 +0300

    Added button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view)
+    {
+        TextView text_my_application = (TextView)findViewById(R.id.text_my_application);
+        text_my_application.setText("Ping!");
     }
 }
