84dc2b322c7b26249193fab72739a0cb7f34618a
bluethea <nazar.lukiashko@knf.stud.vu.lt>
2022-09-07 11:35:19 +0300

    Revert "Change color button"
    This reverts commit 471e444330b6296482d893ea6e49542054027c10.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,26 +2,22 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text_my_application;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text_my_application = (TextView)findViewById(R.id.text_my_application);
     }
 
     /** Called when the user touches the button */
-    public void sendMessage(View view) {
+    public void sendMessage(View view)
+    {
+        TextView text_my_application = (TextView)findViewById(R.id.text_my_application);
         text_my_application.setText("Ping!");
-    }
-
-    public void changeColor(View view) {
-        text_my_application.setTextColor(Color.RED);
     }
 }
